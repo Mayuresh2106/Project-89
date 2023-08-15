9d53a4c9b17c8df7ff01ce59409113feacf8c90a
Mayuresh2106 <88275218+Mayuresh2106@users.noreply.github.com>
2023-08-15 12:59:03 +0530

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9u_BCWYdRWKkTOVZrZL1ioQrJe4gslzk",
+  authDomain: "spectragram-75c04.firebaseapp.com",
+  projectId: "spectragram-75c04",
+  storageBucket: "spectragram-75c04.appspot.com",
+  messagingSenderId: "62982891916",
+  appId: "1:62982891916:web:79b75129569f5bdd858ea9"
+};
